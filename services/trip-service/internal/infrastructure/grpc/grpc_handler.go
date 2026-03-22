cc14245220f6975026f79efe1119715c135bd17c
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2026-03-22 22:16:44 +0100

    trip preview handler implementation

---
+++ services/trip-service/internal/infrastructure/grpc/grpc_handler.go
@@ -0,0 +1,51 @@
+package grpc
+
+import (
+	"context"
+	"log"
+	"ride-sharing/services/trip-service/internal/domain"
+	pb "ride-sharing/shared/proto/trip"
+	"ride-sharing/shared/types"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type gRPCHandler struct {
+	pb.UnimplementedTripServiceServer
+	service domain.TripService
+}
+
+func NewGRPCHandler(server *grpc.Server, service domain.TripService) *gRPCHandler {
+	handler := &gRPCHandler{
+		service: service,
+	}
+	pb.RegisterTripServiceServer(server, handler)
+	return handler
+}
+
+func (h *gRPCHandler) PreviewTrip(ctx context.Context, req *pb.PreviewTripRequest) (*pb.PreviewTripResponse, error) {
+	startLocation := req.GetStartLocation()
+	endLocation := req.GetEndLocation()
+
+	pickup := &types.Coordinate{
+		Latitude:  startLocation.Latitude,
+		Longitude: startLocation.Longitude,
+	}
+	destination := &types.Coordinate{
+		Latitude:  endLocation.Latitude,
+		Longitude: endLocation.Longitude,
+	}
+
+	t, err := h.service.GetRoute(ctx, pickup, destination)
+	if err != nil {
+		log.Println(err)
+		return nil, status.Errorf(codes.Internal, "failed to get routes: %v", err)
+	}
+	return &pb.PreviewTripResponse{
+		Route:     t.ToProto(),
+		RideFares: []*pb.RideFare{},
+	}, nil
+
+}
--- shared/types/types.go
+++ shared/types/types.go
@@ -15,13 +15,4 @@ type (
 		Latitude  float64 `json:"latitude"`
 		Longitude float64 `json:"longitude"`
 	}
-	OsrmApiResponse struct {
-		Routes []struct {
-			Distance float64 `json:"distance"`
-			Duration float64 `json:"duration"`
-			Geometry struct {
-				Coordinates [][]float64 `json:"coordinates"`
-			} `json:"geometry"`
-		} `json:"routes"`
-	}
 )
