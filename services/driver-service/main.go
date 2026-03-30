388f82b181dccbf0680babb81fccb99719277be7
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2026-03-30 22:07:53 +0100

    driver service skaffold

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var GrpcAddr = ":9092"
 
 func main() {
-	log.Printf("Hello world")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		// if we dont do this program will start immediately and
+		// then exit because we didnt receive the signal so we need to block
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", GrpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	service := NewService()
+
+	// Starting the gRPC server
+	grpcServer := grpcserver.NewServer()
+	NewGrpcHandler(grpcServer, service)
+
+	log.Printf("Starting gRPC server Driver service on port %s", lis.Addr().String())
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Printf("failed to serve: %v", err)
+			cancel()
+		}
+
+	}()
+
+	// wait for the shutdown signal
+	<-ctx.Done()
+	log.Println("Shutting down the server...")
+	grpcServer.GracefulStop()
+
 }
